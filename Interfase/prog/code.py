d9edee8341e30d50fecc0e962798ffc9e4454bef
Miniki_Al <miniki2002@yandex.ua>
2022-07-19 22:39:59 +0500

    Work main interface

---
+++ Interfase/prog/code.py
@@ -0,0 +1,16 @@
+from PyQt5 import QtWidgets
+from main import Ui_MainWindow  # импорт нашего сгенерированного файла
+import sys
+
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+
+sys.exit(app.exec())
