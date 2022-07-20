9c770dc010353d1416c91356f4f48021ed8ed70f
BrahamGreedy <94718642+BrahamGreedy@users.noreply.github.com>
2022-07-20 19:59:08 +0500

    2

---
+++ prog/oop/test.py
@@ -0,0 +1,11 @@
+class Data_Transform:
+    def __init__(self, param):
+        self.param = param
+    def calculation(self):
+        #тут вычисление новых значений
+    def read_param(self):
+        #тут считываем значения из таблицы
+    def write_param(self):
+        #тут вписываем значения в таблицу
+    def removing_param(self):
+        #тут удаляем значения из таблицы
