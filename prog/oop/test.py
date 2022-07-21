43f6270ba282adac59a33887cd0241b9e00aeef6
BrahamGreedy <94718642+BrahamGreedy@users.noreply.github.com>
2022-07-21 14:33:23 +0500

    f

--- prog/oop/test.py
+++ prog/oop/test.py
@@ -1,11 +1,31 @@
+import openpyxl
+
 class Data_Transform:
-    def __init__(self, param):
+    def __init__(self, param, row):
         self.param = param
+        self.row = row
     def calculation(self):
         #тут вычисление новых значений
     def read_param(self):
-        #тут считываем значения из таблицы
+        f = openpyxl.load_workbook("datagrafiks2.xlsx")
+        sheet = f["Первый лист"]
+        i = 1
+        for key in self.param:
+            self.param[key] = sheet.cell(row = self.row, column = i)
+            i += 1
+        f.save("datagrafiks2.xlsx")
     def write_param(self):
-        #тут вписываем значения в таблицу
+        f = openpyxl.load_workbook("datagrafiks2.xlsx")
+        sheet = f["Первый лист"]
+        i = 1
+        for key in self.param:
+            sheet.cell(row=self.row, column=i, value=self.param[key])
+            i += 1
+        f.save("datagrafiks2.xlsx")
     def removing_param(self):
-        #тут удаляем значения из таблицы
+        f = openpyxl.load_workbook("datagrafiks2.xlsx")
+        sheet = f["Первый лист"]
+        for i in range(1, 15):
+            sheet.cell(row=self.row, column=i, value="")
+        f.save("datagrafiks2.xlsx")
+        self.param.clear()
