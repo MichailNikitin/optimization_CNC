b25ace56d7af41a23d90da4f64b1670deef691f0
Miniki <miniki2002@yandex.ua>
2022-08-22 10:19:50 +0500

    add read from xlsx

--- Interfase/prog/Graf.py
+++ Interfase/prog/Graf.py
@@ -9,6 +9,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
+import pandas
+excel_data_df = pandas.read_excel('datagrafiks2.xlsx', sheet_name='data1')
+
+ddo = excel_data_df['ddo'].tolist()
+time_cicle = excel_data_df['time_cicle'].tolist()
+
 
 class MplCanvas(FigureCanvasQTAgg):
 
@@ -24,17 +30,6 @@ class MainWindow(QtWidgets.QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         sc = MplCanvas(self, width=5, height=4, dpi=100)
-        ddo = [50.3, 50.2959874693863, 50.2872916714614, 50.2750251438493, 50.260236155994, 50.2437186769883,
-               50.2260348098752,
-               50.2075706363046, 50.1885871349996, 50.1692591051679, 50.1497029749659, 50.1299960246567,
-               50.1101894364898,
-               50.0903170599171, 50.0704012606187, 50.0504568075639, 50.0304934494445, 50.016972041566,
-               50.0075574847718,
-               50.0008105162819, 49.9958306136035, 49.9920449757749, 49.9890827845386]
-        time_cicle = [0, 0.00333333333333333, 0.00666666666666667, 0.01, 0.0133333333333333, 0.0166666666666667, 0.02,
-                      0.0233333333333333, 0.0266666666666667, 0.03, 0.0333333333333333, 0.0366666666666667, 0.04,
-                      0.0433333333333333, 0.0466666666666667, 0.05, 0.0533333333333333, 0.0566666666666667, 0.06,
-                      0.0633333333333333, 0.0666666666666667, 0.07, 0.0733333333333333]
 
         sc.axes.plot(ddo, time_cicle)
 
